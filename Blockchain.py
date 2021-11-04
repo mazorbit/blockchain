8dbfa9b8feb0e5c632558ec78cfea6a91a48a1f3
Hans Yoo <mazorbit@gmail.com>
2021-11-04 12:01:43 +0900

    Adding add_block function

--- Blockchain.py
+++ Blockchain.py
@@ -1,25 +1,30 @@
+from rich.console import Console
+con = Console()
+
+def lightning_hash(data):
+    return data + "*"
+
 class Block:
     def __init__(self, data, hash, last_hash):
         self.data = data
         self.hash = hash
         self.last_hash = last_hash
 
-    def lightning_hash(data):
-        return data + "_*"
-
-# foo_block = Block('Hello', 'hash', 'last_hash')
-
-# print(foo_block.data)
-# print(foo_block.hash)
-# print(foo_block.last_hash)
-
 class Blockchain:
     def __init__(self):
         genenis_block = Block("gen_data", "gen_hash", "gen_last_hash")
         self.chain = [genenis_block]
 
+    def add_block(self, data):
+        last_hash = self.chain[-1].hash
+        this_hash = lightning_hash(data + "_" + last_hash) 
+        new_block = Block(data, this_hash, last_hash)
+        
+        self.chain.append(new_block)
+
 bc = Blockchain()
-print(bc.chain, type(bc.chain))
+bc.add_block("one")
+bc.add_block("two")
 
-
-
+for block in bc.chain:
+    con.print(block.__dict__)
